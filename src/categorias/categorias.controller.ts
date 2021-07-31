cfd5702847408d1b89ff431809bfa84673e5536b
Leonardo Treviso <leonardosistemas@outlook.com>
2021-07-31 15:46:56 -0300

    finalizando a criação do método criar categoria

--- src/categorias/categorias.controller.ts
+++ src/categorias/categorias.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { CategoriasService } from './categorias.service';
+import { CriarCategoriaDto } from './dtos/criar-categoria-dto';
+import { Categoria } from './interfaces/categoria.interface';
 
-@Controller('categorias')
-export class CategoriasController {}
+@Controller('api/v1/categorias')
+export class CategoriasController {
+    constructor(private readonly categoriasServices : CategoriasService){}
+
+    @Post()
+    @UsePipes(ValidationPipe)
+    async criarCategoria(@Body() criarCategoriaDto: CriarCategoriaDto) : Promise<Categoria>{
+
+        return await this.categoriasServices.criarCategoria(criarCategoriaDto)
+    }
+}
