04ebb0a2f804873062f2e165e2dac442c8293612
Leonardo Treviso <leonardosistemas@outlook.com>
2021-07-31 23:23:03 -0300

    referenciando classe de erro no main

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AllExceptionFilter } from './common/filters/http-exception-filters';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalFilters(new AllExceptionFilter())
+
   await app.listen(8080);
 }
 bootstrap();
