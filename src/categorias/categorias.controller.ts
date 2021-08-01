1eb484b96734ebdb1cb14e9e2c70b8142db6b83c
Leonardo Treviso <leonardosistemas@outlook.com>
2021-07-31 22:08:03 -0300

    criando método atualizar categoria no controller

--- src/categorias/categorias.controller.ts
+++ src/categorias/categorias.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put, UsePipes, ValidationPipe } from '@nestjs/common';
 import { CategoriasService } from './categorias.service';
 import { CriarCategoriaDto } from './dtos/criar-categoria-dto';
 import { Categoria } from './interfaces/categoria.interface';
@@ -24,5 +24,10 @@ export class CategoriasController {
     async consultarCategoriaPeloId(@Param('categoria') categoria : string) : Promise<Categoria> {
         
         return await this.categoriasServices.consultarCategoriaPeloId(categoria)
+    }
+
+    @Put('/:categoria')
+    async atualizarCategoria(@Body() atualizarCategoriaDto : AtualizarCategoriaDto, @Param('categoria') categoria : string) : Promise<void> {
+        
     }
 }
